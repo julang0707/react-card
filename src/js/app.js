6bb939aa1feef543f09aaa55c80c5f11f29f110e
Julie Langmade <jlangmade@gmail.com>
2015-08-23 20:53:57 -0500

    added sunday work

--- src/js/app.js
+++ src/js/app.js
@@ -11,7 +11,7 @@ let Property = ReactForms.schema.Property
 class App extends React.Component {
   render() {
     return (
-        <Card name="Your Card"/>
+      <Card name="Your Card"/>
     )
   }
 };
@@ -40,7 +40,7 @@ class Card extends React.Component {
       placeholder='XXXXXXXXXXXX'
       value={value}
       handleChange={this.handleChange.bind(this)}
-    />
+      />
 
     // check for MasterCard
     if (/^5[1-5]/.test(value))
@@ -90,53 +90,42 @@ class Card extends React.Component {
       card = <DinersClub ref="card" value={value} />
     }
 
-    $("input").each(function() {
-      var $this = $(this);
-      $(this).data("maxlength", $this.prop("maxlength"));
-      $(this).removeAttr("maxlength");
-    }).bind("input", function() {
-      var $this = $(this);
-      setTimeout(function() { spill($this, $this.val()); },0);
-    });
-
-    function spill($this, val) {
-      var maxlength = $this.data("maxlength");
-      if ( val.length >= maxlength ) {
-        $this.val(val.substring(0, maxlength));
-        var next = $this.next("input").focus();
-        spill(next, val.substring(maxlength));
-      } else
-        $this.val(val);
-      }
-    };
-
     return (
       <div className="card">
         <h1>{this.props.name}</h1>
         {card}
       </div>
     )
-  };
+  }
+}
 
-};
 
 class DefaultCard extends React.Component {
   render() {
 
     return (
       <input
+        ref="card"
         type="text"
         id="cc-num"
         placeholder='XXXXXXXXXXXX'
         value={this.props.value}
         onChange={this.props.handleChange}
-      />
+        />
     )
   }
 }
 
 class Visa extends React.Component {
   render() {
+    $(document).ready(function(){
+        $('input').keyup(function(){
+            if($(this).val().length==$(this).attr("maxlength")){
+                $(this).next().focus();
+            }
+        });
+    });
+
     return (
       <div className="visa">
         <input id="cc1" defaultValue={this.props.value} size="4" maxLength="4"/>
